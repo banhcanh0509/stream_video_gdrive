13dc45bd14e248954545f8f759767e687537396c
banhcanh0509 <148322810+banhcanh0509@users.noreply.github.com>
2023-10-19 11:17:11 +0700

    Add files via upload
    update code

--- main.js
+++ main.js
@@ -1,57 +1,86 @@
-let api_key = 'AIzaSyB9GI92hqiigUTOZg61O3B9uGVhfqy1e3k';
-let api_key2 = 'AIzaSyCHNqE9putPjoS55mqyIcHJjFWTd3_t5J4';
-let fileId = 'co-cc-ay';
-let folderId = '1IT2v0jCp77Ar5vsPFXHt_rW02avQSZLg';
+const apiUrl = 'https://653086f86c756603295ebc3e.mockapi.io/';
+const apiGoogleDrive = 'https://www.googleapis.com/drive/v2/files';
 
-const urlList = `https://www.googleapis.com/drive/v2/files?q='${folderId}'+in+parents&key=${api_key}
-`;
-const urlList2 = `https://www.googleapis.com/drive/v2/files?q='${folderId}'+in+parents&key=${api_key2};
-`;
+// function call api
+async function fetchAPI(url) {
+    const data = await fetch(url).then((res) => res.json());
+    return data;
+}
 
-const videoEle = document.querySelector('.video');
+// get url apis google
+function getGoogleApiFolder(key, id) {
+    return `${apiGoogleDrive}?q='${id}'+in+parents&key=${key}`;
+}
+
+function getGoogleApiFile(key, id) {
+    return `${apiGoogleDrive}/${id}?&key=${key}`;
+}
+
+// call api to get key for google api
+const keyGoogleApis = await fetchAPI(`${apiUrl}keys`);
+
+// call api to get folders
+const idFolder = await fetchAPI(`${apiUrl}folders`).then((data) => data[0].idFolder);
+
+// Get Elements
 const listVideosEle = document.querySelector('.listVideos');
+const videoEle = document.querySelector('.video');
+videoEle.volume = 0.1;
 
-const videos = await fetch(urlList)
-    .then((res) => {
-        if (res.status - 200 > 100) return new Promise.reject();
-        return res.json();
+// videos stored list of videos in google drive folder
+const videos = await fetchAPI(getGoogleApiFolder(keyGoogleApis[0].key, idFolder))
+    .then((data) => {
+        // if request is success then return list of videos else reject the promise
+        if (!data.error) return data.items;
+        return Promise.reject();
     })
-    .then((data) => data.items)
     .catch(async () => {
-        await fetch(urlList2)
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => data.items);
+        // request the api with another key
+        await fetch(getGoogleApiFolder(keyGoogleApis[1].key, idFolder)).then((data) => data.items);
     });
 
-videos.forEach((item) => {
-    const str = `<li class='linkVideo' idVideo="${item.id}">
-        <p class="idVideo">Id: ${item.id}</p>
-        <p>Title: ${item.title}</p>
-    </li>`;
+// List videos from Google Drive API to list videos Element
+if (!videos) {
+    listVideosEle.innerHTML = `<h2 class='empty'>Empty Video List</h2>`;
+} else {
+    videos.forEach(
+        (video, index) =>
+            (listVideosEle.innerHTML =
+                listVideosEle.innerHTML +
+                `
+            <li class='linkVideo' idVideo="${video.id}">
+            <p><label>${index + 1}.</label> ${video.title}</p>
+            </li>
+            `),
+    );
+}
 
-    listVideosEle.innerHTML = listVideosEle.innerHTML + str;
-});
-
+// get all linkVideo elements which list from Google Drive API
 const links = document.querySelectorAll('.linkVideo');
 
+// addEventListener for each video title element to play and add class active for each one
 links.forEach((link) => {
     link.onclick = () => {
         document.querySelector('.linkVideo.active')?.classList.remove('active');
-        fileId = link.getAttribute('idVideo');
+        const fileId = link.getAttribute('idVideo');
         handleVideoStream(fileId);
         link.classList.add('active');
     };
 });
 
 async function handleVideoStream(idVide) {
-    await fetch(`https://www.googleapis.com/drive/v2/files/${idVide}?&key=${api_key}`).then((res) =>
-        res
-            .json()
-            .then((data) => data.downloadUrl)
-            .then((url) => {
-                videoEle.src = url;
-            }),
-    );
+    await fetchAPI(getGoogleApiFile(keyGoogleApis[0].key, idVide))
+        .then((data) => {
+            if (!data.error) return data.downloadUrl;
+            return Promise.reject();
+        })
+        .then((url) => {
+            videoEle.src = url;
+        })
+        .catch(async () => {
+            // request the api with another key
+            await fetch(getGoogleApiFile(keyGoogleApis[1].key, idVide))
+                .then((data) => data.downloadUrl)
+                .then((url) => (videoEle.src = url));
+        });
 }
